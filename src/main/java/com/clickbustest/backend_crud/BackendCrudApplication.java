0eb240da77d491929f8ed5d4dd4688f5884a34bc
levish <levyceesar@gmail.com>
2021-08-25 20:45:55 -0300

    first commit

---
+++ src/main/java/com/clickbustest/backend_crud/BackendCrudApplication.java
@@ -0,0 +1,13 @@
+package com.clickbustest.backend_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clickbustest/backend_crud/BackendCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clickbustest.backend_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
